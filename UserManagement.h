ec6cf545703d68b03c70938234da604c1411d26a
Ellen Seon <ellen24h@gmail.com>
2014-06-01 23:57:29 +0900

    new

---
+++ UserManagement.h
@@ -0,0 +1,16 @@
+#ifndef  __1STVER_H__
+#define __1STVER_H__
+
+#define MAX_SIZE 100
+
+typedef struct info{
+
+	int id;
+	char name[20];
+	char address[100];
+	char phonenum[20];
+
+}Info;
+
+#endif
+
